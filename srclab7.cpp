f1e93d7fc7c562be12b2fd47f60a73d3cd2c11e9
jojo077501454 <80879309+jojo077501454@users.noreply.github.com>
2021-03-31 15:46:45 +0700

    Update srclab7.cpp

--- srclab7.cpp
+++ srclab7.cpp
@@ -3,10 +3,11 @@
 
 int cnt = 0;
 
-void setup()
+void setup()  
+
 {
-	Serial.begin(115200);
-	WiFi.mode(WIFI_STA);
+	Serial.begin(115200);  #กำหนดความเร็วในการส่งข้อมูล
+	WiFi.mode(WIFI_STA); #ตั้งค่า WiFi เป็นโหมดสถานีและตัดการเชื่อมต่อจากเครื่อข่ายหากเชื่อมต่อไว้ก่อนหน้านี้
 	WiFi.disconnect();
 	delay(100);
 	Serial.println("\n\n\n");
@@ -15,21 +16,22 @@ void setup()
 void loop()
 {
 	Serial.println("========== เริ่มต้นแสกนหา Wifi ===========");
-	int n = WiFi.scanNetworks();
+	int n = WiFi.scanNetworks();   #กำหนดตัวแปร
+	String ssid;
+	int32_t rssi;
+	uint8_t* bssid;
+	int32_t channel;
+
+	
 	if(n == 0) {
 		Serial.println("NO NETWORK FOUND");
 	} else {
 		for(int i=0; i<n; i++) {
-			Serial.print(i + 1);
-			Serial.print(": ");
-			Serial.print(WiFi.SSID(i));
-			Serial.print(" (");
-			Serial.print(WiFi.RSSI(i));
-			Serial.println(")");
-			Serial.print(WiFi.channel(i));
+			WiFi.getNetworkInfo(i, ssid, rssi, bssid, channel); #แสดงข้อมูล WiFi ในรูปแบบของฟังก์ชัน
+			Serial.printf("%d: %s Ch:%d %d %s) \n", i + 1, ssid.c_str(), channel, rssi, bssid);  
 			delay(10);
 		}
 	}
 	Serial.println("\n\n");
-	delay(10 * 1000);
+	delay(1000);
 }
